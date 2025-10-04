869289857b28bc013e29c13fa489ec4b318e14ce
yhthia <e1408755@U.nus.edu>
2025-10-04 17:19:27 +0800

    Fix import order checkstyle errors

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -27,15 +27,15 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.DateOfBirth;
+import seedu.address.model.person.Dependents;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.MaritalStatus;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Occupation;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Salary;
-import seedu.address.model.person.DateOfBirth;
-import seedu.address.model.person.MaritalStatus;
-import seedu.address.model.person.Occupation;
-import seedu.address.model.person.Dependents;
 import seedu.address.model.tag.Tag;
 
 /**
