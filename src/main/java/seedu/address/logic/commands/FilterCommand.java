8822112cd4d3ef6cbb1895477f198d04707581fe
Matthias <103831255+matthias890@users.noreply.github.com>
2025-10-09 20:39:50 +0800

    Add initial structure of filter command

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,4 +1,22 @@
 package seedu.address.logic.commands;
 
-public class FilterCommand {
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class FilterCommand extends Command {
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Filters for relevant entries based on the specifications.\n"
+            + "Parameters: category:\"description\" (must be an existing category)\n"
+            + "Example: filter " + PREFIX_NAME + "\"alex yeoh\" " + PREFIX_ADDRESS + "\"Geylang Street\"";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Filter command not implemented yet";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+    }
 }
