4d72a642f2c6b63efb1feb69c867d6dd951d91f4
Matthias <103831255+matthias890@users.noreply.github.com>
2025-10-09 20:57:57 +0800

    Fix checkstyle violations

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+/**
+ * Filters for persons in the address book based on the specifications to the parameters.
+ */
 public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
 
