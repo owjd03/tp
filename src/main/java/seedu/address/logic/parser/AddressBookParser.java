e3d6e6124e1935d533b89992920f96075635d91f
Jun Duan <e1398588@u.nus.edu>
2025-10-10 12:54:14 +0800

    Update Sort feature

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -79,7 +79,7 @@ public class AddressBookParser {
             return new HelpCommand();
 
         case SortCommand.COMMAND_WORD:
-            return new SortCommand();
+            return new SortCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
