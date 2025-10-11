92ac0663e56563074330b37248da56f41d44f4fc
yhthia <e1408755@U.nus.edu>
2025-10-11 15:46:25 +0800

    Extend add command to support new categories
    The add command only supports editing a small subset of a contact's details (name, phone, email, address).
    
    Our target audience (financial advisors) would want more critical attributes of each client.
    
    Let's,
    * expand AddCommandParser to recognise prefixes for salary, date of birth, marital status, dependents, occupation
    * update the AddCommand logic to apply these changes to a Person
    * enhance the UI to correctly display these newly editable fields

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -7,10 +7,15 @@ import java.util.stream.Collectors;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.DateOfBirth;
+import seedu.address.model.person.Dependents;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.MaritalStatus;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Occupation;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Salary;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -20,23 +25,30 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
+                    new Address("Blk 30 Geylang Street 29, #06-40"), new Salary("3000"), new DateOfBirth("2001-01-01"),
+                    new MaritalStatus("Single"), new Occupation("Engineer"), new Dependents(0),
+                    getTagSet("friends")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Salary("4000"),
+                    new DateOfBirth("1999-02-23"), new MaritalStatus("Married"), new Occupation("Manager"),
+                    new Dependents(2), getTagSet("colleagues", "friends")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
+                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Salary("5000"),
+                    new DateOfBirth("1985-10-12"), new MaritalStatus("Married"),
+                    new Occupation("Director"), new Dependents(3),
+                    getTagSet("neighbours")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Salary("6000"),
+                    new DateOfBirth("1978-05-30"), new MaritalStatus("Divorced"), new Occupation("Consultant"),
+                    new Dependents(1), getTagSet("family")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
+                    new Address("Blk 47 Tampines Street 20, #17-35"), new Salary("7000"), new DateOfBirth("1990-07-15"),
+                    new MaritalStatus("Single"), new Occupation("Analyst"), new Dependents(0),
+                    getTagSet("classmates")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+                    new Address("Blk 45 Aljunied Street 85, #11-31"), new Salary("8000"), new DateOfBirth("1988-11-25"),
+                    new MaritalStatus("Married"), new Occupation("Architect"), new Dependents(4),
+                    getTagSet("colleagues"))
         };
     }
 
--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -11,10 +11,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.DateOfBirth;
+import seedu.address.model.person.Dependents;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.MaritalStatus;
+import seedu.address.model.person.MaritalStatusEnum;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Occupation;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Salary;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -28,6 +34,12 @@ class JsonAdaptedPerson {
     private final String phone;
     private final String email;
     private final String address;
+    private final String salary;
+    private final String dateOfBirth;
+    private final String maritalStatus;
+    private final String occupation;
+    private final int dependents;
+
     private final List<JsonAdaptedTag> tags = new ArrayList<>();
 
     /**
@@ -35,12 +47,21 @@ class JsonAdaptedPerson {
      */
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-            @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+                             @JsonProperty("email") String email, @JsonProperty("address") String address,
+                             @JsonProperty("salary") String salary, @JsonProperty("dateOfBirth") String dateOfBirth,
+                             @JsonProperty("maritalStatus") String maritalStatus,
+                             @JsonProperty("occupation") String occupation,
+                             @JsonProperty("dependents") int dependents,
+                             @JsonProperty("tags") List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.salary = salary;
+        this.dateOfBirth = dateOfBirth;
+        this.maritalStatus = maritalStatus;
+        this.occupation = occupation;
+        this.dependents = dependents;
         if (tags != null) {
             this.tags.addAll(tags);
         }
@@ -54,6 +75,11 @@ class JsonAdaptedPerson {
         phone = source.getPhone().value;
         email = source.getEmail().value;
         address = source.getAddress().value;
+        salary = source.getSalary().value;
+        dateOfBirth = source.getDateOfBirth().value;
+        maritalStatus = source.getMaritalStatus().value;
+        occupation = source.getOccupation().value;
+        dependents = source.getDependents().value;
         tags.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
@@ -102,8 +128,50 @@ class JsonAdaptedPerson {
         }
         final Address modelAddress = new Address(address);
 
+        if (salary == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Salary.class.getSimpleName()));
+        }
+        if (!Salary.isValidSalary(salary)) {
+            throw new IllegalValueException(Salary.MESSAGE_CONSTRAINTS);
+        }
+        final Salary modelSalary = new Salary(salary);
+
+        if (dateOfBirth == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    DateOfBirth.class.getSimpleName()));
+        }
+        if (!DateOfBirth.isValidDateOfBirth(dateOfBirth)) {
+            throw new IllegalValueException(DateOfBirth.MESSAGE_CONSTRAINTS);
+        }
+        final DateOfBirth modelDateOfBirth = new DateOfBirth(dateOfBirth);
+
+        if (maritalStatus == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    MaritalStatus.class.getSimpleName()));
+        }
+        if (!MaritalStatusEnum.isValidMaritalStatus(maritalStatus)) {
+            throw new IllegalValueException(MaritalStatus.MESSAGE_CONSTRAINTS);
+        }
+        final MaritalStatus modelMaritalStatus = new MaritalStatus(maritalStatus);
+
+        if (occupation == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Occupation.class.getSimpleName()));
+        }
+        if (!Occupation.isValidOccupation(occupation)) {
+            throw new IllegalValueException(Occupation.MESSAGE_CONSTRAINTS);
+        }
+        final Occupation modelOccupation = new Occupation(occupation);
+
+        if (dependents < 0) {
+            throw new IllegalValueException(Dependents.MESSAGE_CONSTRAINTS);
+        }
+        final Dependents modelDependents = new Dependents(dependents);
+
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+
+        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelSalary, modelDateOfBirth,
+                modelMaritalStatus, modelOccupation, modelDependents, modelTags);
     }
 
 }
