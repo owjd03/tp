756923890eb2ba577e7d884ab83bd76131ac10f8
Vigo <e1375607@u.nus.edu>
2025-10-21 19:53:18 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -15,7 +15,19 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.*;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.ExportCommand;
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListPackageCommand;
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
@@ -125,6 +137,7 @@ public class AddressBookParserTest {
     public void parseCommand_listPackage() throws Exception {
         assertTrue(parser.parseCommand(ListPackageCommand.COMMAND_WORD) instanceof ListPackageCommand);
         assertTrue(parser.parseCommand(ListPackageCommand.COMMAND_WORD + " 3") instanceof ListPackageCommand);
+
     }
 
     @Test
