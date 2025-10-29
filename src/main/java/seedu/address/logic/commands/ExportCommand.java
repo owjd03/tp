eeba923dcd2979b3e99935d949f95effa667985b
limweekit <e1406546@u.nus.edu>
2025-10-29 20:46:40 +0800

    Add filepath parameter to export command

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -19,11 +20,16 @@ public class ExportCommand extends Command {
 
     public static final String COMMAND_WORD = "export";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports the address book to a CSV file.\n"
-            + "Example: " + COMMAND_WORD;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports the address book to a CSV file."
+            + "You can specify a valid file path and it must end with .csv. "
+            + "If no file path is specified, it will be saved to data/addressbook.csv.\n"
+            + "Parameters: FILE_PATH\n"
+            + "Example (Windows): " + COMMAND_WORD + " C:\\Users\\User\\Downloads\\addressbook.csv\n"
+            + "Example (macOS/Linux): " + COMMAND_WORD + " ~/Downloads/addressbook.csv";
 
     public static final String MESSAGE_SUCCESS = "Address book has been exported to %1$s";
     public static final String MESSAGE_EXPORT_FAILURE = "Error occurred during exporting: %1$s";
+    public static final String MESSAGE_IS_DIRECTORY = "The specified path is a directory, please specify a file path.";
 
     // Default file path for the export.
     private final Path filePath;
@@ -38,6 +44,7 @@ public class ExportCommand extends Command {
 
     /**
      * Creates an ExportCommand to export the address book to the specified location.
+     *
      * @param filePath The path to export the file to.
      */
     public ExportCommand(Path filePath) {
@@ -51,6 +58,11 @@ public class ExportCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        if (Files.isDirectory(filePath)) {
+            throw new CommandException(MESSAGE_IS_DIRECTORY);
+        }
+
         List<Person> personList = model.getAddressBook().getPersonList();
 
         try {
--- src/main/java/seedu/address/logic/parser/ExportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -1,6 +1,11 @@
 package seedu.address.logic.parser;
 
-import seedu.address.logic.Messages;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import seedu.address.logic.commands.ExportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -12,13 +17,30 @@ public class ExportCommandParser implements Parser<ExportCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the ExportCommand
      * and returns an ExportCommand object for execution.
-     * Export command does not take any arguments for now.
+     * @throws ParseException if the user input does not conform to the expected format
      */
     public ExportCommand parse(String args) throws ParseException {
-        if (!args.trim().isEmpty()) {
-            throw new ParseException(
-                    String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE));
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            return new ExportCommand();
         }
-        return new ExportCommand();
+
+        if (!trimmedArgs.toLowerCase().endsWith(".csv")) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE));
+        }
+
+        try {
+            Path filePath;
+            if (trimmedArgs.startsWith("~/") || trimmedArgs.startsWith("~\\")) {
+                String home = System.getProperty("user.home");
+                String remainingPath = trimmedArgs.substring(2);
+                filePath = Paths.get(home, remainingPath);
+            } else {
+                filePath = Paths.get(trimmedArgs);
+            }
+            return new ExportCommand(filePath);
+        } catch (InvalidPathException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE), e);
+        }
     }
 }
