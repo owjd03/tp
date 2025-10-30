632d3115408eafd6703eccc302c5b214aa787c49
Matthias <103831255+matthias890@users.noreply.github.com>
2025-10-31 02:52:29 +0800

    Change FilterTagParser to use a List

--- src/main/java/seedu/address/logic/parser/filter/FilterTagParser.java
+++ src/main/java/seedu/address/logic/parser/filter/FilterTagParser.java
@@ -3,7 +3,8 @@ package seedu.address.logic.parser.filter;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.ParserUtil.parseTags;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,7 +21,7 @@ import seedu.address.model.tag.Tag;
 public class FilterTagParser implements FilterPrefixParser {
 
     private final Prefix prefix;
-    private Set<String> keywords = new HashSet<>();
+    private List<String> keywords = new ArrayList<>();
     private Set<Tag> parsedTags;
 
     /**
